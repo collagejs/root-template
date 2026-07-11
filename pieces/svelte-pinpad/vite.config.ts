339c909295c38af49d307f47811da03803522755
Ramirez Vargas, José Pablo <webJose@gmail.com>
2026-07-10 23:07:18 -0600

    chore: Adjust port numbers

--- pieces/svelte-pinpad/vite.config.ts
+++ pieces/svelte-pinpad/vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { cjsCssPlugin } from '@collagejs/vite-css';
 
+const port = 6101;
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
-    const port = 6100;
     return {
         plugins: [svelte(), cjsCssPlugin({
             serverPort: port,
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { cjsImPlugin } from '@collagejs/vite-im';
 
+const port = 6100;
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [svelte(), cjsImPlugin()],
+    server: {
+        port,
+    },
+    preview: {
+        port,
+    }
 });
